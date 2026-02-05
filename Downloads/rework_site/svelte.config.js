c2c0b40094abf8beeaedcb676a45ab9ceabb6aca
2Mon <1mon3dp@gmail.com>
2026-02-04 19:15:18 -0500

    Fix Vercel adapter + routes

--- Downloads/rework_site/svelte.config.js
+++ Downloads/rework_site/svelte.config.js
@@ -1,20 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
 
-const repoName = 'rework'; 
-
 export default {
   kit: {
     adapter: adapter({
-	  runtime: 'nodejs20.x',
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html' 
+      runtime: 'nodejs20.x'
     }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? `/${repoName}` : ''
-    },
     prerender: {
-  		entries: ['/', '/how-it-works', '/submitting/journaling', '/submitting/submission-guidelines', '/examples'],
+      entries: ['/', '/how-it-works', '/submitting/journaling', '/submitting/submission-guidelines', '/examples'],
       handleHttpError: 'warn'
     }
   }
